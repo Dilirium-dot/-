e34ac208c6322121beabfc636b8803d94873031e
Dily <artempomazanov228@gmail.com>
2025-06-04 14:26:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+words = ["радиостанция", "калиграфия", "калейдоскоп", "пастила", "крокодил", "рентген", "рентгеноэлектрокардиография", "водогрязеторфопарафинолечение", "опоссум", "лололошка точка невозврата 71", "мультимедиавидеоконтроллер", "скриптонит", "патологоанатом", ""]
+import random
+
+word = words[random.randint(0, len(words)) - 1]
+vis = '*' * len(word)
+word_list = list(word)
+vis_list = list(vis)
+count = 0
+print(vis)
+while True:
+    count += 1
+    ans = input("Введите букву: ")
+    if ans in word:
+        print('Есть такая буква')
+        for i in range(0, len(word_list)):
+            if word_list[i] == ans:
+                vis_list[i] = ans
+    else:
+        print('Нет такой буквы')
+    print(''.join(vis_list), 'Попытка', count)
+    if '*' in vis_list:
+        continue
+    else:
+        break
